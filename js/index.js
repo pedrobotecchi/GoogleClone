996c09e9bb8f1b00def0af265794542653664314
pedrohb29 <pedrobotecchi@gmail.com>
2021-05-05 13:46:17 -0300

    Adding JS

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// Function that will get the string to search
+function setSearch(e) {
+    search = document.getElementById('userSearch').value;
+    if(!e.key || e.key === 'Enter') {
+        if(search !== '') {
+            window.location.href = "http://127.0.0.1:5500/searchResult.html";
+            localStorage.setItem("search", search);
+        }
+    }
+}
+
+// Function that loads the value of search and set them in the desired positions
+function loadSearch() {
+    const search = localStorage.getItem("search");
+    document.getElementById("userSearch").value = search;
+    document.getElementById("searchSpan").innerHTML = search;
+
+    // get all related boxes to change the innerHTML
+    const boxes = Array.prototype.slice.call(document.getElementsByClassName("related__text"));
+    boxes.map(boxText => {
+        const newInnerHTML = getNewText(boxText.innerHTML, search);
+        document.getElementById(boxText.id).innerHTML = newInnerHTML;
+    });
+}
+
+// This function returns a new string to insert into the related values, based on search text
+function getNewText(text, search) {
+    const split = ' ';
+    const strings = text.split(split);
+    
+    return search + split + strings[strings.length -1];
+}
+
+// Function that return the placeholder into Search input
+function unSetSearch() {
+    document.getElementById("userSearch").value = null;
+}
+
+// Function to show when a button in nav bar is clicked
+function showAlert(){
+    alert("JS is commanding HuEhUe");
+}
